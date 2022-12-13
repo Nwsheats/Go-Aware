f259799a3d7ddc355b34052df2856fc0a1e2a8a3
Nwsheats <nwsheats@gmail.com>
2022-12-12 23:15:05 -0600

    changed front end js files to use search button with dropdowns, commented out some divs on dashboard, added the beginnings of logout functionality

--- public/js/stateslived.js
+++ public/js/stateslived.js
@@ -3,32 +3,48 @@ const livedInFormHandler = async (event) => {
     event.preventDefault();
   
     // Gather the data from the form elements on the page
-    const stateLived = document.querySelector('#lived-in').value.trim();
-    const statesVisited = document.querySelector('#visited').value.trim();
-    const visitState = document.querySelector('#visit').value.trim();
-    console.log(stateLived, statesVisited, visitState);
-  
-    // if (stateLived) {
-    //   // Send info the to the server
-    //   console.log(stateLived, "states");
-      const response = await fetch('/api/userstates', {
+    const stateLived = document.querySelector('#lived-in')
+    const statesVisited = document.querySelector('#visited')
+    const visitState = document.querySelector('#visit')
+    console.log(stateLived.value.trim(), statesVisited.value.trim(), visitState.value.trim(), "lived");
+
+    
+
+    const response = await fetch('/api/userstates', {
         method: 'POST',
         body: JSON.stringify({ 
-            stateLived,
-            statesVisited,
-            visitState
+            stateLived: stateLived.value.trim(),
+            statesVisited: statesVisited.value.trim(),
+            visitState: visitState.value.trim()
              }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        
+        // insert Leaflet function
       }
+
+      stateLived.selectedIndex = 0
+      statesVisited.selectedIndex = 0
+      visitState.selectedIndex = 0
     }
 //   };
 
+document
+    .querySelector('.info')
+    .addEventListener('change', function() {
+      const stateLived = document.querySelector('#lived-in').value.trim();
+      const statesVisited = document.querySelector('#visited').value.trim();
+      const visitState = document.querySelector('#visit').value.trim();
+      if (stateLived !== "" || statesVisited !== "" || visitState !== "") {
+        document.getElementById("visit-search").disabled = false;
+      }
+    });
+
+
+  document.getElementById("visit-search").disabled = true;
 
   document
-    .querySelector('#lived-in')
-    .addEventListener('select', livedInFormHandler);
-  
+    .querySelector('#visit-search')
+    .addEventListener('click', livedInFormHandler);
+
